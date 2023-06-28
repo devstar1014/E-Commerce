3025cea129e5c71afd3f3754c482b6ecbc63f214
devstar1014 <mercurys1030@gmail.com>
2023-06-28 19:40:16 +0300

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { ShoppingCart } from 'phosphor-react';
+import './Navbar.css';
+import logo from '../assets/logo.png'; 
+
+export const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='logo'>
+                <Link to="/">
+                    <img src={logo}/>
+                </Link>
+            </div>
+            <div className='links'>
+                <Link to="/"> SHOP </Link>
+                <Link to="/cart">
+                    <ShoppingCart size={32} />
+                </Link>
+      
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
